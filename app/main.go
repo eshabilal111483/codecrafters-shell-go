d50c5822dec931a438c1683ead157ed5032bbddf
Esha <esha.bilal111@gmail.com>
2026-08-06 20:25:44 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -33,6 +34,12 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
+		}
+
 		fmt.Println(command[:len(command)-1] + ": command not found")
 
 	}
