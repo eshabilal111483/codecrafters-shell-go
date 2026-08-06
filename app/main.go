8f9ef163745a5f687d8fd80b23c87337e099bd3b
Esha <esha.bilal111@gmail.com>
2026-08-06 19:54:05 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,9 +5,21 @@ import (
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+//var _ = fmt.Print
 
 func main() {
+
+	var command string
+
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	_, err := fmt.Scan(command)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	_ = fmt.Sprintf("%v: command not found", command)
+
 }
